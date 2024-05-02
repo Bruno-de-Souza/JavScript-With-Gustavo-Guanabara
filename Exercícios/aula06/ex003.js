edb5927c76dd20ecd46ff2858a6f6c96f9b0374f
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-01 23:31:54 -0300

    Refactor file paths and update styles in ex001.html and ex001.js

---
+++ Exercícios/aula06/ex003.js
@@ -0,0 +1,4 @@
+var n1 = Number(window.prompt('Digite o primeiro número: '));
+var n2 = Number(window.prompt('Digite o segindo número: '));
+var s = n1 + n2;
+window.alert(`A soma entre ${n1} e ${n2} é igual a ${s}!`);
