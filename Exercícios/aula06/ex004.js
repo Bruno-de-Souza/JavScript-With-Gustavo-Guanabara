110956e662b3f305b52b567590dd38c165c6819e
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-01 23:35:49 -0300

    Refactor ex004.js to include currency formatting for n1 variable

--- Exercícios/aula06/ex004.js
+++ Exercícios/aula06/ex004.js
@@ -2,3 +2,13 @@ var nome = window.prompt('Qual é o seu nome?');
 document.write(`Olá, ${nome}! Seu nome tem ${nome.length} letras. <br/>`);
 document.write(`Seu nome em maiúsculo é ${nome.toUpperCase()}. <br/>`);
 document.write(`Seu nome em minúsculo é ${nome.toLowerCase()}. <br/>`)
+
+
+/*
+
+n1 1545.5
+n1.toFixed(2)  "1545.50"
+n1.toFixed(2).replace('.', ',')  "1545,50"
+n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})  "R$ 1.545,50"
+
+*/
