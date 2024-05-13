4e8466def264501c8fda8a04aeb8a3cbd34f3897
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-12 22:58:56 -0300

    Conditions in JS

---
+++ Exercícios/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = '60';
+var res = Number(vel);
+var c = '"Cinto de Segurança"';
+console.log(`A velocidade do seu carro é ${res}Km/h!`);
+if (res > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida! Multado!`);
+}
+console.log(`Dirija sempre com ${c}!`);
