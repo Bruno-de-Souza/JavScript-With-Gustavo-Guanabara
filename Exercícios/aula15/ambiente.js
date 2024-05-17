d74a14410534358ca4685165815ec0f5749022bb
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-16 21:32:03 -0300

    Estruturas de Repetição

---
+++ Exercícios/aula15/ambiente.js
@@ -0,0 +1,28 @@
+/*
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
+
+//Estrutura de repetiçao While
+
+/*
+var n = 1;
+while (n <= 6) {
+    console.log(`Passo "While" ${n}`);
+    n = n + 1;
+}
+console.log(`Quantidade de passos "While" é: ${n - 1}`);
+*/
+
+//Estrutura de repetição Do WHile
+
+var n = 1;
+ do {
+    console.log(`Passo "Do - While" ${n}`);
+    n++;
+ } while (n <= 6);
+ console.log(`Quantidade de passos "Do - While" é: ${n - 1}`);
---
+++ Exercícios/aula15/node_62ce622299553.tmp.js
@@ -0,0 +1,25 @@
+/*console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+console.log('Tudo bem?');
+*/
+
+//Estrutura de repetiçao While
+
+/*
+var n = 1;
+while (n <= 6) {
+    console.log(`Passo "While" ${n}`);
+    n = n + 1;
+}
+console.log(`Quantidade de valores impressos é ${n - 1}`);
+*/
+
+//Estrutura de repetição Do WHile
+
+var n = 1;
+ do {
+    console.log(`Passo "Do - While" ${n}`);
+ } while (n <= 6);
