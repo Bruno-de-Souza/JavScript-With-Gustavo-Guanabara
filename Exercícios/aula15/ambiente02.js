da59834233b1f8ca12e5fde957f0e7fe0bc36b09
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-18 00:18:26 -0300

    Estrutura de repetição "For". Usando a ferramenta de depuração nativa do VSCode.

---
+++ Exercícios/aula15/ambiente02.js
@@ -0,0 +1,7 @@
+//Estrutura de repetiçao com variavél de controle - For
+console.log('Vai começar...');
+for (var i = 0; i <=10; i++) {
+    console.log(`Passo "For" de número ${i}!`);
+}
+console.log('FIM!');
+console.log(`Total de passos "For" é: ${i - 1}`);
---
+++ Exercícios/aula15/node_67a032e5659a4.tmp.js
@@ -0,0 +1,5 @@
+//Estrutura de repetiçao com variavél de controle - For
+var c = 1;
+for (var i = 0; c <=10; i++) {
+    console.log(`Passo "For" de número ${i}`);
+}
