0d39adf7cab06115be52813422f1222e6553156e
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-21 00:49:38 -0300

    Adding emojis to my contar() function

--- Exercícios/aula16/ex016.js
+++ Exercícios/aula16/ex016.js
@@ -1,14 +1,17 @@
 function contar() {
     var txtIn = document.getElementById('txtIn');
     var txtFim = document.getElementById('txtFim');
-    var txtPasso = document.querySelector('div#txtPasso');
+    var txtPasso = document.getElementById('txtPasso');
     var res = document.querySelector('div#res');
     if (txtIn.value == null || txtIn.value == "") {
         res.innerHTML = `Impossível contar!`;
     } else { 
-        var total = 0;
+        var total = '';
+        var emoji = String.fromCodePoint(0x1F449);
+        var emoji2 = String.fromCodePoint(0x1F3C1);
         for (var i = Number(txtIn.value); i <= Number(txtFim.value); i += Number(txtPasso.value)){
-            total += `${i}`;      
+            total += `${i} ${emoji}`;
         }
+        res.innerHTML = `${total} ${emoji2}`;
     }
 }
