6cd3a15c2277a6f3837ada0cf2075fb16a4853c8
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-21 23:53:30 -0300

    Update exercise 17 with tabuada functionality

--- Exercícios/aula16/ex017.js
+++ Exercícios/aula16/ex017.js
@@ -1,1 +1,14 @@
-alert('Olá, Mundo!');
+function calcular() {
+    var num = document.getElementById('num');
+    var res = document.querySelector('div#res');
+
+    if (num.value == null || num.value == "" || num.value == 0) {
+        window.alert('[Erro!] Valor inválido!');
+    } else {
+        var total = '';
+        for (var i = 1; i <= 10; i++) {
+            total += `${num.value} x ${i} = ${num.value * i}` + '<br>';
+        }
+        res.innerHTML = `${total}`;
+    }
+ }
