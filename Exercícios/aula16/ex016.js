6323717fdcdc925316c0690d25bd03ff99b6be4f
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-22 22:07:32 -0300

    Updated ex016.js

--- Exercícios/aula16/ex016.js
+++ Exercícios/aula16/ex016.js
@@ -3,7 +3,7 @@ function contar() {
     var txtFim = document.getElementById('txtFim');
     var txtPasso = document.getElementById('txtPasso');
     var res = document.querySelector('div#res');
-    if (txtIn.value == null || txtIn.value == "") {
+    if (txtIn.value == null || txtIn.value == "" || txtIn.value == 0 || txtFim.value == null || txtFim.value == "" || txtFim.value == 0) {
         res.innerHTML = `Impossível contar!`;
     } else if (txtPasso.value == null || txtPasso.value == "" || txtPasso.value == 0) {
         window.alert('[Erro] Passo Inválido! Considerando Passo 1!');
@@ -12,6 +12,17 @@ function contar() {
         var emoji = String.fromCodePoint(0x1F449);
         var emoji2 = String.fromCodePoint(0x1F3C1);
         for (var i = Number(txtIn.value); i <= Number(txtFim.value); i += Number(txtPasso.value)){
+            total += `${i} ${emoji}`;
+        }
+        for (var i = Number(txtIn.value); i > Number(txtFim.value); i = i - Number(txtPasso.value)) {
+            total = total + `${i} ${emoji}`;
+        }
+        res.innerHTML = `${total} ${emoji2}`;
+    } else if (txtIn.value > txtFim.value) {
+        var total = '';
+        var emoji = String.fromCodePoint(0x1F449);
+        var emoji2 = String.fromCodePoint(0x1F3C1);
+        for (var i = Number(txtIn.value); i > Number(txtFim.value); i = i - Number(txtPasso.value)) {
             total += `${i} ${emoji}`;
         }
         res.innerHTML = `${total} ${emoji2}`;
