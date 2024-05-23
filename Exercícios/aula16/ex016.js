962ed2045dd6c4e2b533db5deaa7fc1a5cb1f1c2
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-22 22:42:26 -0300

    Add emoji functionality to contar() function

--- Exercícios/aula16/ex016.js
+++ Exercícios/aula16/ex016.js
@@ -3,21 +3,24 @@ function contar() {
     var txtFim = document.getElementById('txtFim');
     var txtPasso = document.getElementById('txtPasso');
     var res = document.querySelector('div#res');
+    //Validando os valores inseridos
     if (txtIn.value == null || txtIn.value == "" || txtIn.value == 0 || txtFim.value == null || txtFim.value == "" || txtFim.value == 0) {
         res.innerHTML = `Impossível contar!`;
     } else if (txtPasso.value == null || txtPasso.value == "" || txtPasso.value == 0) {
         window.alert('[Erro] Passo Inválido! Considerando Passo 1!');
         txtPasso.value = 1;
         var total = '';
-        var emoji = String.fromCodePoint(0x1F449);
-        var emoji2 = String.fromCodePoint(0x1F3C1);
+        var emoji = '\u{1F449}';
+        var emoji2 = '\u{1F3C1}';
+        //Contagem crescente
         for (var i = Number(txtIn.value); i <= Number(txtFim.value); i += Number(txtPasso.value)){
             total += `${i} ${emoji}`;
         }
+        //Contagem decrescente
         for (var i = Number(txtIn.value); i > Number(txtFim.value); i = i - Number(txtPasso.value)) {
             total = total + `${i} ${emoji}`;
         }
-        res.innerHTML = `${total} ${emoji2}`;
+        res.innerHTML = `Contando: <br> ${total} ${emoji2}`;
     } else if (txtIn.value > txtFim.value) {
         var total = '';
         var emoji = String.fromCodePoint(0x1F449);
@@ -25,7 +28,7 @@ function contar() {
         for (var i = Number(txtIn.value); i > Number(txtFim.value); i = i - Number(txtPasso.value)) {
             total += `${i} ${emoji}`;
         }
-        res.innerHTML = `${total} ${emoji2}`;
+        res.innerHTML = `Contando: <br> ${total} ${emoji2}`;
     } else { 
         var total = '';
         var emoji = String.fromCodePoint(0x1F449);
@@ -33,6 +36,6 @@ function contar() {
         for (var i = Number(txtIn.value); i <= Number(txtFim.value); i += Number(txtPasso.value)){
             total += `${i} ${emoji}`;
         }
-        res.innerHTML = `${total} ${emoji2}`;
+        res.innerHTML = `Contando: <br> ${total} ${emoji2}`;
     }
-}
+} 
