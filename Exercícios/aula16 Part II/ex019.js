cc3d6e08873365ecc932e1e978dad279e5e530de
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-25 00:49:30 -0300

    feat: Refactor tabuada generation logic in exercise 19

--- Exercícios/aula16 Part II/ex019.js
+++ Exercícios/aula16 Part II/ex019.js
@@ -1,20 +1,18 @@
 function gerar() {
     let numero = document.getElementById('txtNum');
-    let tab = document.getElementById('tabuada');
     let num = Number(numero.value);
-    let resultado = document.getElementById('res');
     if (num == null || num == '' || num == 0) {
         window.alert('Valor Invalído!');
     } else {
         let myNodeP = document.createElement('p');
         let myNodeText = document.createTextNode('Resultado: ');
         myNodeP.appendChild(myNodeText);
-        resultado.appendChild(myNodeP);
-        tab.innerHTML = '';
+        document.getElementById('res').appendChild(myNodeP);
+        document.getElementById('tabuada').innerHTML = '';
         for (var i = 1; i <=10; i++) {
             let myNode = document.createElement('option');
             myNode.text += `${num} X ${i} = ${num * i}`;
-            tab.appendChild(myNode);
+            document.getElementById('tabuada').appendChild(myNode);
         }
     }
 }
