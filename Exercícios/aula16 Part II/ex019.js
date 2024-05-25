66b6c9f54918f55baf5958a533d3eac0e280d045
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-24 23:35:06 -0300

    Function gerar() - DOM Element appendChild()

--- Exercícios/aula16 Part II/ex019.js
+++ Exercícios/aula16 Part II/ex019.js
@@ -2,7 +2,14 @@ function gerar() {
     let numero = document.getElementById('txtNum');
     let tab = document.getElementById('tabuada');
     let num = Number(numero.value);
-    for (var i = 1; i <=10; i++) {
-        tab.innerHTML += `${num} X ${i} = ${num * i} <br>`;
+    if (num == null || num == '' || num == 0) {
+        window.alert('Valor Invalído!');
+    } else {
+        tab.innerHTML = '';
+        for (var i = 1; i <=10; i++) {
+            let myNode = document.createElement('option');
+            myNode.text += `${num} X ${i} = ${num * i}`;
+            tab.appendChild(myNode);
+        }
     }
 }
