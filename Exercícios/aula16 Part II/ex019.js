5584c0320b1caa79befaf45fd87e21e9d1340736
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-25 00:45:36 -0300

    feat: Add tabuada result display to exercise 19

--- Exercícios/aula16 Part II/ex019.js
+++ Exercícios/aula16 Part II/ex019.js
@@ -2,9 +2,14 @@ function gerar() {
     let numero = document.getElementById('txtNum');
     let tab = document.getElementById('tabuada');
     let num = Number(numero.value);
+    let resultado = document.getElementById('res');
     if (num == null || num == '' || num == 0) {
         window.alert('Valor Invalído!');
     } else {
+        let myNodeP = document.createElement('p');
+        let myNodeText = document.createTextNode('Resultado: ');
+        myNodeP.appendChild(myNodeText);
+        resultado.appendChild(myNodeP);
         tab.innerHTML = '';
         for (var i = 1; i <=10; i++) {
             let myNode = document.createElement('option');
