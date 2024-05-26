69f02dfd5501c5d229a353a6d2c9cf94f8ea67ca
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-26 00:22:45 -0300

    feat: Add code for displaying and manipulating an array in exercise 17

---
+++ Exercícios/aula17/ambiente.js
@@ -0,0 +1,32 @@
+let numeros = [8, 5, 2, 9, 3];
+numeros.sort();
+console.log(`Meu vetor é o : ${numeros}!`);
+/*
+console.log(`Meu vertor ordernado crescente é ${numeros.sort()}`);
+numeros[5] = 8;
+console.log(`Meu vetor após adicionar um valor na última posição: ${numeros}!`);
+numeros.push(6);
+console.log(`Meu vetor após utilizar o método push ${numeros}!`);
+console.log(`Minha quantidade de elementos no meu vetor é ${numeros.length}!`);
+console.log(`O primeiro valor do vetor é: ${numeros[0]}`);
+*/
+/*
+for (let pos = 0; pos < numeros.length; pos++) {
+    console.log(`A posiçao ${pos} tem o valor ${numeros[pos]}`);
+}
+*/
+
+//Só podemos utilizar o "for in" em objetos e arrays no JS
+for (let pos in numeros) {
+    console.log(`A posiçao ${pos} tem o valor ${numeros[pos]} no meu array usando o "For in"!`);
+}
+
+//Utilizando o indexOf
+console.log(`Meu indídice de valor 9 no meu array é: ${numeros.indexOf(9)}`);
+
+let value = numeros.indexOf(0);
+if (value == -1) {
+    console.log('Valor nao encontrado!');
+} else {
+    console.log(`O valor está na posição ${value}`);
+}
