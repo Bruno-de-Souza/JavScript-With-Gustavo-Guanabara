9f8a147c90c678318692faab19bead3de37ce3ae
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-27 23:23:10 -0300

    feat: Add functionality to display and manipulate an array in exercise 17

--- Exercícios/aula18/analisador.js
+++ Exercícios/aula18/analisador.js
@@ -1,3 +1,16 @@
-function analisador (n) {
- return n
+function add() {
+    let num = window.document.getElementById('num');
+    let res = window.document.getElementById('res');
+    let values = [];
+    let n = Number(num.value);
+    if (n == null || n == '' || n <= 0) {
+        window.alert('[ERRO] Valor inválido!');
+    } else {
+        res.innerHTML = '';
+        for (let i = 0; i <= n; i++) {
+            let node = document.createElement('option');
+            node.text += `O número ${i} foi adicionado!`;
+            res.appendChild(node); 
+        }
+    }
 }
