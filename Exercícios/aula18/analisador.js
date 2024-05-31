c47994483ceb9c6d79b295ab370f4b413dec8cd2
Bruno Moreira <brunomoreiramg@gmail.com>
2024-05-31 01:10:57 -0300

    Update Analisador de Números page with improved functionality

--- Exercícios/aula18/analisador.js
+++ Exercícios/aula18/analisador.js
@@ -1,28 +1,70 @@
-var num = window.document.getElementById('num');
-function add() {
-    let res = window.document.getElementById('res');
-    let values = new Array(100);
-    let index = 0;
-    let resp = '';
-    let n = Number(num.value);
-    if (n == null || n == '' || n <= 0) {
-        window.alert('[ERRO] Valor inválido!');
+let num = document.querySelector('input#num');
+let res = document.querySelector('select#res');
+let resII = document.getElementById('resII');
+let valores = [];
+
+//Verifica se o número está no valor exigido
+function isNumero(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true;
     } else {
-        if (index < values.length) {
-            values[index] = n;
-            index++;
-        } else {
-            window.alert('Limite de valores atingidos!');
-        }
-        for (let i = 0; i < index; i++) {
-            resp += values[i];
-            let node = document.createElement('option');
-            node.text = `Valor ${resp} foi adicionado!`;
-            res.appendChild(node);
-        }
+        return false;
     }
 }
 
+//Verifica se o número está dentro da lista
+function inlista(n, l) { 
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//Valida os valores inseridos
+function add() {
+    if (isNumero(num.value) && !inlista(num.value, valores)) {
+        valores.push(Number(num.value));
+        console.log(`Valores: ${valores}`);
+        let node = document.createElement('option');
+        node.text = `Valor ${num.value} adicionado!`;
+        res.appendChild(node);
+        resII.innerHTML = '';
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista!');
+    }
+    num.value = '';
+    num.focus();
+}
+
+//Valida a funcionalidade do botao Finalizar
 function finalizar() {
-    
+    if (valores.length == 0) {
+        window.alert('[ERRO!] Nenhum valor adicionado!');
+    } else {
+        //Declaraçâo das variavéis
+        let total = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        //Varifica os valores da lista após finalizar
+        for (let pos in valores) {
+            soma += valores[pos];
+            media = valores[pos] / 2;
+            if (valores[pos] > maior) {
+                maior = valores[pos];
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos];
+            }
+        }
+        resII.innerHTML = '';
+        resII.innerHTML += `<p>Números cadastrados: ${total}.<p/>`;
+        resII.innerHTML += `<p>Maior número informado: ${maior}.<p/>`;
+        resII.innerHTML += `<p>Menor número informado: ${menor}<p/>`;
+        resII.innerHTML += `<p>Soma dos valores: ${soma}<p/>`;
+        resII.innerHTML += `<p>Média dos valores: ${media}<p/>`;
+    }
 }
